3b38e1159e43ef7e25597a1cbbe67b5f7356785f
PremVaidhya <102459558+PremVaidhya@users.noreply.github.com>
2023-02-28 19:17:22 +0530

    Create avatar.js

---
+++ src/commands/PrefixCommand/Utility/avatar.js
@@ -0,0 +1,8 @@
+module.exports = {
+   name: "avatar",
+   code: `$title[$username[$mentioned[1;yes]'s Avatar]
+$image[$userAvatar[$mentioned[1;yes]]]
+$color[FF0000]
+$footer[Requested By $username;$userAvatar]
+$addTimestamp`
+}
