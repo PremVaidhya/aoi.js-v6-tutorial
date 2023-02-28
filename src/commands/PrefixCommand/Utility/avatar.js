a41e93ed75696027947ca67b1a58c5fd07f2d501
PremVaidhya <102459558+PremVaidhya@users.noreply.github.com>
2023-02-28 19:27:52 +0530

    Delete src/commands/PrefixCommand directory

--- src/commands/PrefixCommand/Utility/avatar.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-   name: "avatar",
-   code: `$title[$username[$mentioned[1;yes]'s Avatar]
-$image[$userAvatar[$mentioned[1;yes]]]
-$color[FF0000]
-$footer[Requested By $username;$userAvatar]
-$addTimestamp`
-}
