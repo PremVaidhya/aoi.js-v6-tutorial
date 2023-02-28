6233e3d1993fa4d060d077d1c5dda05149251e5a
PremVaidhya <102459558+PremVaidhya@users.noreply.github.com>
2023-02-28 19:27:35 +0530

    Delete src/commands/SlashCommand directory

--- src/commands/SlashCommand/Utility/avatar.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-   name: "avatar",
-   type: "interaction",
-   prototype: "slash",
-   code: `
-$interactionReply[;{newEmbed:{title:$username[$mentioned[1;yes]]'s Avatar}{image:$userAvatar[$mentioned[1;yes]]}{color:FF0000}{footer:Requested By $username:$userAvatar}{timestamp}}]`
-}
